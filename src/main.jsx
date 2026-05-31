c33a0a70b18a1be792286336d950ef047483525b
Vahidsk18 <vahidvadu098@gmail.com>
2026-05-31 15:35:46 +0530

    Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider>
         <BrowserRouter>
             <App />
+            <Analytics />
         </BrowserRouter>
     </ThemeProvider>
 
